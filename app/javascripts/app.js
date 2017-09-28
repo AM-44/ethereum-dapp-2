9f3437e4b9282933bacb2043f6ddfd93bbf34937
alexander <alex@alexandermichael.co.ke>
2017-09-28 11:47:29 +0000

    modified webpack host config

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -32,7 +32,7 @@ window.voteForCandidate = function(candidate) {
      * everywhere we have a transaction call
      */
     Voting.deployed().then(function(contractInstance) {
-      contractInstance.voteForCandidate(candidateName, {gas: 140000, from: web3.eth.accounts[0]}).then(function() {
+      contractInstance.voteForCandidate(candidateName, {gas: 500000, from: web3.eth.accounts[0]}).then(function() {
         let div_id = candidates[candidateName];
         return contractInstance.totalVotesFor.call(candidateName).then(function(v) {
           $("#" + div_id).html(v.toString());
